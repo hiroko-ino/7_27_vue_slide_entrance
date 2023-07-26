af7b98704051ce1113a7439949a984652d04c4d5
hiroko-ino <los73.butterfly.1251.12.15@gmail.com>
2023-07-26 18:00:53 +0900

    一旦完成

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,33 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+import { useBattery, useAnimate, useBluetooth } from '@vueuse/core'
+import DemoCard from './components/DemoCard.vue';
+
+import useBatteryImage from './assets/useBattery.png'
+import useAnimateImage from './assets/useAnimate.png'
+import HeartRate from './components/HeartRate.vue';
+
+const el = ref(null)
+
+const { isSupported: batteryIsSupported } = useBattery()
+const { isSupported: animateIsSupported } = useAnimate(el, {})
+const { isSupported: bluetoothIsSupported } = useBluetooth()
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <main class="p-6">
+    <h1 class="text-center text-2xl font-bold">@Vue.jsの勉強会はなんぼあってもいいですからね 7/27 デモ集</h1>
+    <div class="flex flex-col gap-y-5 mt-8 justify-center max-w-xl m-auto">
+      <DemoCard text="useBattery" :img="useBatteryImage" link="https://hiroko-ino.github.io/vuejs--vueuse-battery-night-and-sun/" :isSupported="batteryIsSupported" />
+      <DemoCard text="useAnimate" :img="useAnimateImage" link="https://hiroko-ino.github.io/vuejs--vueuse-battery-night-and-sun/" :isSupported="animateIsSupported" />
+      <div class="rounded-xl drop-shadow-md bg-emerald-600 text-white p-5" target="_blank">
+        <h2 class="text-xl mb-3"># useBluetooth</h2>
+        <HeartRate />
+        <p class="mt-2">白ポチでBluetooth通信が始まります</p>
+        <p class="">{{ batteryIsSupported ? 'お使いのブラウザでサポートされています' : 'お使いのブラウザでサポートされていません' }}</p>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
   </main>
 </template>
 
---
+++ src/components/DemoCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps({
+  img: {
+    type: String
+  },
+  text: {
+    type: String
+  },
+  isSupported: {
+    type: Boolean
+  },
+  link: {
+    type: String,
+  }
+})
+
+</script>
+
+<template>
+  <a :href="link" class="rounded-xl drop-shadow-md bg-emerald-600 text-white p-5" target="_blank">
+    <h2 class="text-xl mb-3"># {{ props.text }}</h2>
+    <img class="object-cover w-full h-40 rounded-2xl" :src="img">
+    <p class="mt-2">{{ isSupported ? 'お使いのブラウザでサポートされています' : 'お使いのブラウザでサポートされていません' }}</p>
+  </a>
+</template>
